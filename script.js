91f5fc8a88b8080ef112d3462aba8075fc5d7d09
mumtozBEK1234 <email manzilingiz>
2024-05-16 17:53:40 +0300

    4 oy 1 dars

---
+++ script.js
@@ -0,0 +1,20 @@
+// 1
+// let L = 100
+// console.log(L/100)
+
+
+
+// 2
+// let M = 1000
+// console.log(M/1000)
+
+// 3
+// let kb = 1024
+// console.log(kb/1024)
+
+// 4
+// savolga tushunmadim 
+
+// 5
+
+
